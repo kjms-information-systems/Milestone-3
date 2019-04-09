c3a9e6dad27dfecab35e127104cad56cf05377e7
kmlindgren10 <34612438+kmlindgren10@users.noreply.github.com>
2019-04-09 10:44:12 -0600

    Update vbp.php

--- model/vbp.php
+++ model/vbp.php
@@ -5,33 +5,69 @@ class Vbp extends \Model {
         $provider_number = '060034';
 		//change $filename parameter to the provider_number
 		$safety = \DB::select('*')->from('test_safety')->where('provider_number', '=', $provider_number)->execute();
+		$tps = \DB::select('*')->from('test_TPS')->where('provider_number', '=', $provider_number)->execute();
+		$clinical_care = \DB::select('*')->from('test_clinical_care')->where('provider_number', '=', $provider_number)->execute();
 		
 		$safety_array = array();
 		$safety_array = $safety->as_array();
 		
+		$tps_array = array();
+		$tps_array = $tps->as_array();
 		
+		$clinical_care_array = array();
+		$clinical_care_array = $clinical_care->as_array();
 		
 		$csv = array();
 		$file = fopen($filename, "r");
 		
-		//have these fgetcsvs to [$result_array[0]['provider_num'], $result_array[0]['...'],...]
-		//use same format for arr data
-		
+		//SAFETY
 		$csv['psi90'] = [$safety_array[0]['psi_90_achievement_threshold'], $safety_array[0]['psi_90_benchmark'],$safety_array[0]['psi_90_baseline_rate'],$safety_array[0]['psi_90_performance_rate'],$safety_array[0]['psi_90_achievement_points'],$safety_array[0]['psi_90_improvement_points'],$safety_array[0]['psi_90_measure_score']] ;
 		
+		
+		$csv['ha1'] = [$safety_array[0]['hai_1_achievement_threshold'], $safety_array[0]['hai_1_benchmark'],$safety_array[0]['hai_1_baseline_rate'],$safety_array[0]['hai_1_performance_rate'],$safety_array[0]['hai_1_achievement_points'],$safety_array[0]['hai_1_improvement_points'],$safety_array[0]['hai_1_measure_score']] ;
+
+		$csv['ha2'] = [$safety_array[0]['hai_2_achievement_threshold'], $safety_array[0]['hai_2_benchmark'],$safety_array[0]['hai_2_baseline_rate'],$safety_array[0]['hai_2_performance_rate'],$safety_array[0]['hai_2_achievement_points'],$safety_array[0]['hai_2_improvement_points'],$safety_array[0]['hai_2_measure_score']] ;
+		
+		$csv['ha3'] = [$safety_array[0]['hai_3_achievement_threshold'], $safety_array[0]['hai_3_benchmark'],$safety_array[0]['hai_3_baseline_rate'],$safety_array[0]['hai_3_performance_rate'],$safety_array[0]['hai_3_achievement_points'],$safety_array[0]['hai_3_improvement_points'],$safety_array[0]['hai_3_measure_score']] ;
+		
+		$csv['ha4'] = [$safety_array[0]['hai_4_achievement_threshold'], $safety_array[0]['hai_4_benchmark'],$safety_array[0]['hai_4_baseline_rate'],$safety_array[0]['hai_4_performance_rate'],$safety_array[0]['hai_4_achievement_points'],$safety_array[0]['hai_4_improvement_points'],$safety_array[0]['hai_4_measure_score']] ;
+		
+		$csv['ha5'] = [$safety_array[0]['hai_5_achievement_threshold'], $safety_array[0]['hai_5_benchmark'],$safety_array[0]['hai_5_baseline_rate'],$safety_array[0]['hai_5_performance_rate'],$safety_array[0]['hai_5_achievement_points'],$safety_array[0]['hai_5_improvement_points'],$safety_array[0]['hai_5_measure_score']] ;
+		
+		$csv['ha6'] = [$safety_array[0]['hai_6_achievement_threshold'], $safety_array[0]['hai_6_benchmark'],$safety_array[0]['hai_6_baseline_rate'],$safety_array[0]['hai_6_performance_rate'],$safety_array[0]['hai_6_achievement_points'],$safety_array[0]['hai_6_improvement_points'],$safety_array[0]['hai_6_measure_score']] ;
+		
+		$csv['pc01'] = [$safety_array[0]['pc_01_achievement_threshold'], $safety_array[0]['pc_01_benchmark'],$safety_array[0]['pc_01_baseline_rate'],$safety_array[0]['pc_01_performance_rate'],$safety_array[0]['pc_01_achievement_points'],$safety_array[0]['pc_01_improvement_points'],$safety_array[0]['pc_01_measure_score']] ;
+		
+		$csv['combined_ssi'] = [$safety_array[0]['combined_ssi_measure_score']];
+		
+		$csv['safety_tps'] = [$tps_array[0]['unweighted_safety'], $tps_array[0]['unweighted_safety'], $tps_array[0]['weighted_safety']];
+		
+		//CLINICAL CARE
+		$csv['mortAMI'] = [$clinical_care_array[0]['mort_30_ami_achievement_threshold'], $clinical_care_array[0]['mort_30_ami_benchmark'],$clinical_care_array[0]['mort_30_ami_baseline_rate'],$clinical_care_array[0]['mort_30_ami_performance_rate'],$clinical_care_array[0]['mort_30_ami_achievement_points'],$clinical_care_array[0]['mort_30_ami_improvement_points'],$clinical_care_array[0]['mort_30_ami_measure_score']] ;
+		
+		$csv['mortHF'] = [$clinical_care_array[0]['mort_30_hf_achievement_threshold'], $clinical_care_array[0]['mort_30_hf_benchmark'],$clinical_care_array[0]['mort_30_hf_baseline_rate'],$clinical_care_array[0]['mort_30_hf_performance_rate'],$clinical_care_array[0]['mort_30_hf_achievement_points'],$clinical_care_array[0]['mort_30_hf_improvement_points'],$clinical_care_array[0]['mort_30_hf_measure_score']] ;
+		
+		$csv['mortPN'] = [$clinical_care_array[0]['mort_30_pn_achievement_threshold'], $clinical_care_array[0]['mort_30_pn_benchmark'],$clinical_care_array[0]['mort_30_pn_baseline_rate'],$clinical_care_array[0]['mort_30_pn_performance_rate'],$clinical_care_array[0]['mort_30_pn_achievement_points'],$clinical_care_array[0]['mort_30_pn_improvement_points'],$clinical_care_array[0]['mort_30_pn_measure_score']] ;
+		
+		$csv['cc_tps'] = [$tps_array[0]['unweighted_clinical_care'], $tps_array[0]['unweighted_clinical_care'], $tps_array[0]['weighted_clinical_care']];
+		
+		
 		$csv['test'] = fgetcsv($file, 1000, ",");
-		$csv['ha1'] = fgetcsv($file, 1000, ",");
-		$csv['ha2'] = fgetcsv($file, 1000, ",");
-		$csv['ha3'] = fgetcsv($file, 1000, ",");
-		$csv['ha4'] = fgetcsv($file, 1000, ",");
-		$csv['ha5'] = fgetcsv($file, 1000, ",");
-		$csv['ha6'] = fgetcsv($file, 1000, ",");
-		$csv['pc01'] = fgetcsv($file, 1000, ",");
-		$csv['safety_tps'] = fgetcsv($file, 1000, ",");
-		$csv['mortAMI'] = fgetcsv($file, 1000, ",");
-		$csv['mortHF'] = fgetcsv($file, 1000, ",");
-		$csv['mortPN'] = fgetcsv($file, 1000, ",");
-		$csv['cc_tps'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		$csv['test'] = fgetcsv($file, 1000, ",");
+		
+		
+		
 		$csv['MSPB'] = fgetcsv($file, 1000, ",");
 		$csv['efficiency_tps'] = fgetcsv($file, 1000, ",");
 		$csv['nurses'] = fgetcsv($file, 1000, ",");
